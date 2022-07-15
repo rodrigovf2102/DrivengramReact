02bae058f3066646d822f0f37db7fba819f686bf
rodrigo vieira fonseca <rodrigovf2102@gmail.com>
2022-07-15 14:56:29 -0300

    adicionado array de objetos e map para renderizar posts do instagram

--- src/CorpoEsquerdaPosts.js
+++ src/CorpoEsquerdaPosts.js
@@ -1,77 +1,27 @@
+import CorpoEsquerdaPost from "./CorpoEsquerdaPost";
 export default function CorpoEsquerdaPosts() {
+    const posts = [
+        {
+            imgUsuario: "./img/meowed.svg",
+            nomeUsuario: "meowed",
+            imgConteudo: "./img/gato-telefone.svg",
+            imgCurtida: "./img/respondeai.svg",
+            curtidoPor: "respondeai",
+            quantidadeCurtidas: "outras 101.523 pessoas"
+        },
+        {
+            imgUsuario: "./img/barked.svg",
+            nomeUsuario: "barked",
+            imgConteudo: "./img/dog.svg",
+            imgCurtida: "./img/adorable_animals.svg",
+            curtidoPor: "adorable_animals",
+            quantidadeCurtidas: "outras 99.159 pessoas"
+        }
+    ]
     return (
         <div class="posts">
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src="./img/meowed.svg" />
-                        meowed
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                    <img src="./img/gato-telefone.svg" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="./img/respondeai.svg" />
-                        <div class="texto">
-                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src="./img/barked.svg" />
-                        barked
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="conteudo">
-                    <img src="./img/dog.svg" />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="./img/adorable_animals.svg" />
-                        <div class="texto">
-                            Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {posts.map(post => <CorpoEsquerdaPost imgUsuario={post.imgUsuario} nomeUsuario={post.nomeUsuario} imgConteudo={post.imgConteudo} 
+            imgCurtida={post.imgCurtida} curtidoPor={post.curtidoPor} quantidadeCurtidas={post.quantidadeCurtidas} />)}
         </div>
     );
 }
